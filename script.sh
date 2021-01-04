0f6a2b6231100a562f0ca5592d23eeae7666d555
himanshu007-creator <65963997+himanshu007-creator@users.noreply.github.com>
2021-01-04 23:55:30 +0530

    source code

---
+++ script.sh
@@ -0,0 +1,15 @@
+echo HI , Im am himanshu.
+sleep 1
+echo i love to learn new things and i believe everything is possible
+sleep 0.8
+echo I love open-source. Lets get connected
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+echo HERE YOU GO!!
+sleep 0.5
+explorer "https://github.com/himanshu007-creator"
+explorer "https://www.linkedin.com/in/himanshu-here/"
